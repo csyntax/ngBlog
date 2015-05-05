13246d505801b35f14f5fffdeabf01ddf1015fd2
Ivan Cvetomirov Ivanov <slayer_word_11@outlook.com>
2015-05-05 22:27:07 +0300

    build app and remove ts

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,6 @@
 module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        typescript: {
-            base: {
-                src: ['scripts/*.ts'],
-                dest: 'scripts/',
-                options: {
-                    module: 'amd',
-                    target: 'es5',
-                    basePath: "scripts",
-                    sourceMap: false,
-                    declaration: true,
-                    removeComments: true,
-                    references: [
-                        "scripts/ts/jquery.d.ts",
-                        "scripts/ts/angular.d.ts", 
-                        "scripts/ts/angular-route.d.ts",
-                        "scripts/ts/showdown.d.ts"
-                    ]
-                }
-            }
-        },
         watch: {
             files: "*"
         },
@@ -43,7 +23,6 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-open');
-    grunt.loadNpmTasks('grunt-typescript');
 
-    grunt.registerTask('default', ["typescript",'connect', 'open', 'watch']);
+    grunt.registerTask('default', ['connect', 'open', 'watch']);
 }
---
+++ scripts/services.js
@@ -0,0 +1,28 @@
+app.factory('Config', function ($http, $rootScope) {
+    return {
+        make: function () {
+            this.getConfigFile(function (data) {
+                $rootScope.config = data[0];
+            });
+        },
+        getConfigFile: function (callback) {
+            $http.get('config.json').success(callback);
+        }
+    };
+});
+app.factory('Posts', function ($http) {
+    return {
+        getPosts: function (callback) {
+            $http.get('/posts/posts.json').success(callback);
+        },
+        getBySlug: function (slug, callback) {
+            this.getPosts(function (data) {
+                for (var i = 0; i < data.length; i++) {
+                    if (data[i].slug === slug) {
+                        callback(data[i]);
+                    }
+                }
+            });
+        }
+    };
+});
