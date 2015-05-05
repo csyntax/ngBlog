f635c8f1c74d1ea41eaf17470e582eedd25de8c5
Ivan Cvetomirov Ivanov <slayer_word_11@outlook.com>
2015-05-05 22:18:10 +0300

    update core and main controller

--- scripts/controllers.ts
+++ scripts/controllers.ts
@@ -1,11 +1,11 @@
-app.controller('Main', function ($scope, $window, $routeParams, Posts, Config) {
+app.controller("Main", function ($scope, $window, $routeParams, Posts, Config) {
   $scope.Config = Config;
   $scope.Posts = Posts.query;
 
   Config.make();
 
   $scope.$on("$routeChangeSuccess", function($currentRoute, $previousRoute) {
-      if( $routeParams.postTitle ) {
+      if($routeParams.postTitle) {
         Posts.getBySlug($routeParams.postTitle, function(post) {
           $scope.post = post;
           $window.document.title = $scope.post.title + " - " + $scope.config.title;
