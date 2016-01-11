80ba32040848a633d967d87589afbb56ec965f8e
csyntax <csyntax@outlook.com>
2016-01-11 20:30:07 +0200

    project add new controlers

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -18,3 +18,23 @@ app.controller("Main", function ($scope, $window, $routeParams, Config, Posts) {
         }
     });
 });
+
+app.controller("BaseController", ["$scope", "Config", function ($scope, Config) {
+    $scope.Config = Config;
+
+    Config.make();
+}]);
+
+app.controller("PostsController", ["$scope", "Posts",function ($scope, Posts) {
+    
+
+    Posts.getPosts(function (posts) {
+        $scope.posts = posts;
+    });
+}]);
+
+app.controller("PostController",["$scope", "$routeParams", "Posts", function ($scope, $routeParams, Posts) {
+     Posts.getBySlug($routeParams.postTitle, function (post) {
+        $scope.post = post;
+    });
+}]);
